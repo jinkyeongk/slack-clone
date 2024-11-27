3392dbf7e815e75991cc16d860e5792e202a8037
jinkyeongk <jinkyeongk@naver.com>
2024-11-28 00:00:24 +0900

    회원가입페이지 개발

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -4,16 +4,27 @@ import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   
-    const [email] = useState('');
-    const [nickname] = useState('');
-    const [password] = useState('');
-    const [passwordCheck] = useState('');
+    const [email,setEmail] = useState('');
+    const [nickname,setNickname] = useState('');
+    const [password,setPassword] = useState('');
+    const [passwordCheck,setPasswordCheck] = useState('');
     
-    const onChangeEmail = useCallback(()=> {}, []);
-    const onChangeNickname = useCallback(()=> {}, []);
-    const onChangePassword = useCallback(()=> {}, []);
-    const onChangePasswordCheck = useCallback(()=> {}, []);
-    const onSubmit = useCallback(()=> {}, []);
+    const onChangeEmail = useCallback((e)=> {
+        setEmail(e.target.value);
+    }, []);
+    const onChangeNickname = useCallback((e)=> {
+        setNickname(e.target.value);
+    }, []);
+    const onChangePassword = useCallback((e)=> {
+        setPassword(e.target.value);
+    }, []);
+    const onChangePasswordCheck = useCallback((e)=> {
+        setPasswordCheck(e.target.value);
+    }, []);
+    const onSubmit = useCallback((e)=> {
+        e.preventDefault();
+        //console.log(email,nickname,password,passwordCheck);
+    }, [email,nickname,password,passwordCheck]);
   
     return (
       <div id="container">
